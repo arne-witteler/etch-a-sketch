9ae57b8959f8101f9a6f835ac4abd08cf97215f0
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-05-29 10:22:41 +0200

    add buttons for grid size adjustment and clearing, update grid creation logic

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let container = document.getElementById("container");
+const resizeGridButton = document.querySelector("#new-grid-button");
+const clearGridButton = document.querySelector("#clear-button");
+
+createGrid(16);
 
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
@@ -9,13 +13,32 @@ function getRandomColor() {
   return color;
 }
 
-for (let i = 0; i < 16 * 16; i++) {
-  let smallDiv = document.createElement("div");
-  smallDiv.classList.add("small__div");
-  smallDiv.setAttribute("id", `square${i}`);
-  container.append(smallDiv);
+function createGrid(gridSize) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let smallDiv = document.createElement("div");
+    smallDiv.classList.add("small__div");
+    smallDiv.setAttribute("id", `square${i}`);
+    smallDiv.style.width = `${100 / gridSize}%`;
+    smallDiv.style.height = `${100 / gridSize}%`;
+    container.append(smallDiv);
 
-  smallDiv.addEventListener("mouseover", (event) => {
-    smallDiv.style.backgroundColor = getRandomColor();
-})
+    smallDiv.addEventListener("mouseover", () => {
+      smallDiv.style.backgroundColor = getRandomColor();
+    });
+  };
 }
+
+resizeGridButton.addEventListener("click", () => {
+  let gridSize = prompt("Enter a number between 2 and 100:");
+
+  if (gridSize >= 2 && gridSize <= 100) {
+    container.innerHTML = "";
+    createGrid(gridSize);
+  } 
+});
+
+clearGridButton.addEventListener("click", () => {
+  const allSquares = document.querySelectorAll('.small__div');
+
+  allSquares.forEach((square) => square.style.backgroundColor = "white"); 
+});

ace8621cd14666131c06fb2d0c7fc7d761d97fe4
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-05-29 09:31:45 +0200

    implement random color generation on mouseover and adjust grid size for small divs

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 let container = document.getElementById("container");
 
-for (let i = 0; i < 4 * 4; i++) {
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+for (let i = 0; i < 16 * 16; i++) {
   let smallDiv = document.createElement("div");
   smallDiv.classList.add("small__div");
   smallDiv.setAttribute("id", `square${i}`);
   container.append(smallDiv);
+
+  smallDiv.addEventListener("mouseover", (event) => {
+    smallDiv.style.backgroundColor = getRandomColor();
+})
 }
